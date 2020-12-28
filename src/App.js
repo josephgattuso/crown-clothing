7da1582c05129d337cbd3b0bbe32e7a07215fe07
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-12-28 10:21:48 +0000

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Link to="/crown-clothing">Home</Link>
+      <Link to="/crown-clothing/hats">Hats</Link>
+      <Switch>
+        <Route exact path="/crown-clothing" component={HomePage} />
+        <Route path="/crown-clothing/hats" component={HatsPage} />
+      </Switch>
+    </Router>
   );
 }
 
